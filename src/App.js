f1078586d5761500ff7bd5cbe818bd035d7ea7dd
Jakub Przybysz <kuba.kubis@interia.pl>
2022-05-30 21:01:52 +0200

    Added navbar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import { render } from "react-dom";
 import { StrictMode, useState } from "react";
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import ThemeContext from "./components/ThemeContext";
+import Header from "./components/Header";
 import Home from "./pages/Home";
 import Book from "./pages/Book";
 
@@ -11,9 +12,7 @@ const App = () => {
     <StrictMode>
       <ThemeContext.Provider value={theme}>
         <BrowserRouter>
-          <header>
-            <Link to="/">Adopt Me!</Link>
-          </header>
+          <Header />
           <Routes>
             <Route path="/book/:id" element={<Book />} />
             <Route path="/" element={<Home />} />
